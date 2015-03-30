c9c7e3c570561e2b60b43786e8ac2af2257f3fd4
Donny Hurley <donny.hurley@nuim.ie>
2015-03-30 12:08:01 +0100

    Update ROSInteract.java

--- GenerateROS/Java/ie/nuim/cs/dri/interactROS/ROSInteract.java
+++ GenerateROS/Java/ie/nuim/cs/dri/interactROS/ROSInteract.java
@@ -10,19 +10,5 @@ public class ROSInteract {
    private native int ROSCountNodes(long neoId);
    private native detailednode[] GetNeo4jGraphNodes(long neoId);
    private native detailednode[] GetNeo4jGraphNodes(String longid);
-   
-    public static void main(String[] args) {
-		//Long lala = new ROSInteract().GenerateROS("test.csv","ABCDEFGHIJ");
-		int[] b = new int[3];
-		b[0] = 1; b[1] = 2; b[2] = 3;
-		detailednode[] nodes = new ROSInteract().GetNeo4jGraphNodes(2);
-		System.out.println("b0 " + b[0] + " b1 " + b[1] + " b2 " + b[2]);
-		System.out.println("Node stuff " + nodes[0].nodeId + " another : " + nodes[1].nodeId);
-		System.out.println("I'm Java : " + nodes.length);
-		System.out.println("1 : " + nodes[0].inNodeIds.length);
-		System.out.println("2 : " + nodes[0].outNodeIds.length);
-		System.out.println("3 : " + nodes[0].Word);
-		
-    }
     
 }
