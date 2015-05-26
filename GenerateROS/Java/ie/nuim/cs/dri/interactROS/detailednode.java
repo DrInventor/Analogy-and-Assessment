37166e9b51563698ffacac0fdd62767078e8195b
Donny <dhurley@cs.nuim.ie>
2015-05-26 17:03:10 +0100

    no message

--- GenerateROS/Java/ie/nuim/cs/dri/interactROS/detailednode.java
+++
@@ -1,9 +0,0 @@
-package ie.nuim.cs.dri.interactROS;
-
-public class detailednode{
-	long nodeId, GraphId, sentenceId;
-	int type;
-	String Word;
-	int	inDegree, outDegree, degree;
-	long inNodeIds[], outNodeIds[];
-};
